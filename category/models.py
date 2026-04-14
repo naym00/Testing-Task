345984a81a4d69b562fe88e61261f6c8a1df9cc6
naym00 <naymhsain00@gmail.com>
2026-04-15 00:00:15 +0600

    Base of Testing Task

---
+++ category/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f'{self.id} - {self.title}'
---
+++ category/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from category import views
+
+urlpatterns = [
+    # path('add-example/<int:word_id>/', views.add_example, name='add-example'),
+]
