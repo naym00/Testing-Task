ace47ff85c24ad01e364f1ff35159b2cfc427b4f
naym00 <naymhsain00@gmail.com>
2026-04-15 01:29:03 +0600

    Article and SubCategory models added

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from category import models as MODELS_CATE
 
 # Register your models here.
+
+admin.site.register([
+    MODELS_CATE.Category,
+    MODELS_CATE.Article,
+    MODELS_CATE.SubCategory
+])
--- category/models.py
+++ category/models.py
@@ -8,3 +8,36 @@ class Category(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return f'{self.id} - {self.title}'
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='articles'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+class SubCategory(models.Model):
+    title = models.CharField(max_length=100)
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+        related_name='subcategories'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('title', 'article')
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
